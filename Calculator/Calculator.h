16a44da654a8e2b87b55899ca590f9f0897b8ef9
sylviareset <1052251409@qq.com>
2016-03-24 12:54:15 +0800

    code finish

--- Calculator/Calculator.h
+++ Calculator/Calculator.h
@@ -2,22 +2,23 @@
 #include<queue>
 using namespace std;
 
-
 //Scan类
-class Scan{
-    public:
-        Scan(){};  //默认构造函数
-        ~Scan(){};  //默认析构函数
-        bool ToStringQueue(string input);  //读取输入的四则运算表达式
-        queue<string> ScanQueue;
-    private:
+class Scan
+{
+	public:
+		Scan() {}; //默认构造函数
+		~Scan() {}; //默认析构函数
+		bool ToStringQueue(string input);  //读取输入的四则运算表达式
+		queue<string> ScanQueue;
+	private:
 };
 
 //Print类
-class Print{
-    public:
-        Print(){};  //默认构造函数
-        ~Print(){}; //默认析构函数
-        void PrintScanStr(queue<string> ScanQueue);  //遍历队列并打印
-    private:
+class Print
+{
+	public:
+		Print() {}; //默认构造函数
+		~Print() {}; //默认析构函数
+		void PrintScanStr(queue<string> ScanQueue);  //遍历队列并打印
+	private:
 };
--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -5,13 +5,17 @@
 using namespace std;
 int main(void)
 {
-    Scan scan;
-    Print print;
-    string input;
-    getline(cin,input);
-    if(scan.ToStringQueue(input))
-        print.PrintScanStr(scan.ScanQueue);
-    else
-        cout<<"The input is wrong,please input numbers less then 11"<<endl;
-    return 0;
+	Scan scan;
+	Print print;
+	string input;
+	getline(cin,input);
+	if(scan.ToStringQueue(input))
+	{
+		print.PrintScanStr(scan.ScanQueue);
+	}
+	else
+	{
+		cout<<"The input is wrong,please input numbers less then 11"<<endl;
+	}
+	return 0;
 }
